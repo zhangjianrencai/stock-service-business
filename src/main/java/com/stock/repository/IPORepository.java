b268c7052c88a646ad7bcfcfeda8c3b13ed21812
zhangjianrencai <1025962108@qq.com>
2020-05-12 08:41:01 +0800

    change return type to Object

--- src/main/java/com/stock/repository/IPORepository.java
+++ src/main/java/com/stock/repository/IPORepository.java
@@ -1,7 +1,5 @@
 package com.stock.repository;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,6 +11,6 @@ import com.stock.entity.IPO;
 public interface IPORepository extends JpaRepository<IPO, Long> {
 	
 	@Query(value = "select c.company_name as companyName, c.ceo as CEO, c.directors as broadMember, c.brief as remarks, s.sector_name as sectorName from company c left join sector s on c.sector_id = s.id where c.id=?;", nativeQuery = true)
-	public int searchIPOByUser(@Param("id")Long id);
+	public Object searchIPOByUser(@Param("id")Long id);
 
 }
